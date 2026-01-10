c37160175f8c813b6b4f877452d6a8e98c3f4181
Ahmad Mohammadi <ahmmoh0210@gmail.com>
2026-01-10 15:01:34 +0100

    should return a specific booking by id is green

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -5,6 +5,32 @@ type Params = {
     params: { id: string };
 };
 
+// GET /api/bookings/:id
+export async function GET(
+    req: Request,
+    { params }: Params
+) {
+    try {
+        const booking = await db.booking.findUnique({
+            where: { id: params.id },
+        });
+
+        if (!booking) {
+            return NextResponse.json(
+                { succes: false, message: "Booking not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(booking, { status: 200 });
+    } catch {
+        return NextResponse.json(
+            { success: false, message: "Failed to fetch booking"},
+            { status: 500 }
+        );
+    }
+}
+
 // DELETE /api/bookings/:id
 export async function DELETE(
     req: Request,
